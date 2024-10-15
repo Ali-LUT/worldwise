8f1752656e0858289763fb7aa17e68f52bf270b3
Ali Ranjbar <ali.ranjbar.lian@gmail.com>
2024-10-15 19:47:20 +0300

    fixed a small bug

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -12,7 +12,7 @@ function CityItem({ city }) {
   const { cityName, emoji, date, id } = city;
   return (
     <li>
-      <Link className={styles.cityItem} to="{`${id}`}">
+      <Link className={styles.cityItem} to={`${id}`}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
